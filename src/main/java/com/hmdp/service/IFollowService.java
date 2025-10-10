53f349122ebe2586866bba8ce8ec5b2e997a3ebd
FryeLun <fjl894750370@gmail.com>
2025-10-10 10:26:03 +0800

    好友关注

--- src/main/java/com/hmdp/service/IFollowService.java
+++ src/main/java/com/hmdp/service/IFollowService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -8,9 +9,15 @@ import com.baomidou.mybatisplus.extension.service.IService;
  *  服务类
  * </p>
  *
- * @author 虎哥
- * @since 2021-12-22
+ * @author Frye
+ * @since 2025-10-9
  */
 public interface IFollowService extends IService<Follow> {
+
+    Result follow(Long followUserId, Boolean isFollow);
+
+    Result isFollow(Long followUserId);
+
+    Result followCommons(Long id);
 
 }
